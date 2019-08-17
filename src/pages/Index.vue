9c13e9f48b44c2d30de821bfb457171e2415024c
Damon Muma <damon.muma@londonlife.com>
2019-08-17 12:58:19 -0400

    sinnnntax

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,147 +1,179 @@
 <template>
-<div class="homepage">
-  <header class="site-intro" :class="{showInfo: showInfo}">
-    <h1>emergen<span class="t">t</span><wbr><span class="dot" aria-hidden>⋅</span>seas</h1>
-    <p class="tagline">
-      <!-- creating original,
+  <div class="homepage">
+    <header
+      class="site-intro"
+      :class="{showInfo: showInfo}"
+    >
+      <h1>
+        emergen
+        <span class="t">t</span>
+        <wbr />
+        <span
+          class="dot"
+          aria-hidden
+        >⋅</span>seas
+      </h1>
+      <p class="tagline">
+        <!-- creating original,
       undisciplined 
       performances, expressions
-      and collaborations -->
-      theatre &nbsp; art &nbsp; performance <br>&nbsp;or whatever
-      <br>
-      <span>original, collaborative, undisciplined</span>
-    </p>
-    <button :class="{on: showInfo}" @click="toggleInfo" aria-label="what was that? tell me more.">{{ showInfo ? 'shhh! ^':'????? ⌄' }}</button>
-      <div v-if="showInfo" :style="{color: 'fff'}">
-        <p>
-          We're a pretty slow paced group of word, movement and theatre artists making stuff as life allows.
-        </p>
-        <p>
-          We focus on creating new works, punching tradition, and we delight in finding practitioners from other fields to collaborate with.
-        </p>
-        <p>
-          We are based in London, Ontario.
-        </p>
+        and collaborations-->
+        theatre &nbsp; art &nbsp; performance
+        <br />&nbsp;or whatever
+        <br />
+        <span>original, collaborative, undisciplined</span>
+      </p>
+      <button
+        :class="{on: showInfo}"
+        @click="toggleInfo"
+        aria-label="what was that? tell me more."
+      >{{ showInfo ? 'shhh! ^':'????? ⌄' }}</button>
+      <div
+        v-if="showInfo"
+        :style="{color: 'fff'}"
+      >
+        <p>We're a pretty slow paced group of word, movement and theatre artists making stuff as life allows.</p>
+        <p>We focus on creating new works, punching tradition, and we delight in finding practitioners from other fields to collaborate with.</p>
+        <p>We are based in London, Ontario.</p>
       </div>
-  </header>
-  <main>
-  <section class="work">
-    <h2 class="section-title">Work</h2>
-    <ul class="item-list-work">
-      <li class="box">
-        <h3 class="item-title">My Planet and Me</h3>
-        <p>A speculative solo performance about a disaffected young man who believes he has been contacted by a sentient planet.</p>
-        <ul class="version-list">
-          <li><a href="./my-planet-fringe">at 2017 London Fringe</a></li>
-          <li><a href="./my-planet-aeolian">Aeolian Hall collaborative redux</a></li>
-          <li><a href="https://www.youtube.com/watch?v=lYFZSF-NJiM">Short film</a></li>
+    </header>
+    <main>
+      <section class="work">
+        <h2 class="section-title">Work</h2>
+        <ul class="item-list-work">
+          <li class="box">
+            <h3 class="item-title">My Planet and Me</h3>
+            <p>A speculative solo performance about a disaffected young man who believes he has been contacted by a sentient planet.</p>
+            <ul class="version-list">
+              <li>
+                <a href="./my-planet-fringe">at 2017 London Fringe</a>
+              </li>
+              <li>
+                <a href="./my-planet-aeolian">Aeolian Hall collaborative redux</a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/watch?v=lYFZSF-NJiM">Short film</a>
+              </li>
+            </ul>
+          </li>
+          <li class="box">
+            <h3 class="item-title">The Dramatic Weather Agency</h3>
+            <p>
+              An interactive animated poem, illustrated by
+              <a href="http://ahpi.ca">Antony Hare</a>.
+            </p>
+            <!-- <a href="/dramatic-weather-unit">Visit project</a> -->
+            <p>
+              <em>2019ish</em>
+            </p>
+          </li>
         </ul>
-      </li>
-      <li class="box">
-        <h3 class="item-title">The Dramatic Weather Agency</h3>
-        <p>An interactive animated poem, illustrated by <a href="http://ahpi.ca">Antony Hare</a>.</p>
-        <!-- <a href="/dramatic-weather-unit">Visit project</a> -->
-        <p><em>2019ish</em></p>
-      </li>
-    </ul>
-  </section>
+      </section>
 
-  <section class="team">
-    <h2 class="section-title">Team</h2>
-    <ul class="item-list-team box">
-      <li>Kristin Bennett</li>
-      <li>Cara Moyer</li>
-      <li>Damon Muma</li>
-    </ul>
-  </section>
-  <section class="words">
-    <h2 class="section-title">Words</h2>
-    <ul class="item-list-words box">
-      <li><em>En route.</em></li>
-    </ul>
-  </section>
-
-</main>
-  <footer>
-    <p class="small">
-      site copyright and designed rights deserved etc etc
-    </p>
-  </footer>
+      <section class="team">
+        <h2 class="section-title">Team</h2>
+        <ul class="item-list-team box">
+          <li>Kristin Bennett</li>
+          <li>Cara Moyer</li>
+          <li>Damon Muma</li>
+        </ul>
+      </section>
+      <section class="words">
+        <h2 class="section-title">Words</h2>
+        <ul class="item-list-words box">
+          <li>
+            <em>En route.</em>
+          </li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <p class="small">site copyright and designed rights deserved etc etc</p>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'EmergentSeas',
+    title: "EmergentSeas",
     htmlAttrs: {
-      class: 'index' 
+      class: "index"
     },
-    // <script src="//console.re/connector.js" data-channel="YOUR-CHANNEL-NAME" id="consolerescript"></script>
     script: [
-      { src: '//console.re/connector.js', id: 'consolerescript', 'data-channel':'emergent-seas' }
+      {
+        src: "//console.re/connector.js",
+        id: "consolerescript",
+        "data-channel": "emergent-seas"
+      }
     ]
   },
-  data(){
+  data() {
     return {
       showInfo: false
-    }
+    };
   },
 
   methods: {
-    toggleInfo: function(){
-      this.showInfo = !this.showInfo
+    toggleInfo: function() {
+      this.showInfo = !this.showInfo;
     }
   },
-  mounted(){
+  mounted() {
     var h = document.documentElement,
       b = document.body,
-      st = 'scrollTop',
-      sh = 'scrollHeight',
+      st = "scrollTop",
+      sh = "scrollHeight",
       scroll;
 
     const setScrollVars = function() {
-      scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight);
-      document.documentElement.style.setProperty('--scroll', scroll );
-      document.documentElement.style.setProperty('--scrollPct', scroll * 100 + '%' );
-    }
+      scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight);
+      document.documentElement.style.setProperty("--scroll", scroll);
+      document.documentElement.style.setProperty(
+        "--scrollPct",
+        scroll * 100 + "%"
+      );
+    };
 
-    document.addEventListener('scroll', setScrollVars);
+    document.addEventListener("scroll", setScrollVars);
     setScrollVars();
 
-
-    document.addEventListener("mousemove", (e) => {
-      document.documentElement.style.setProperty('--mouseX', e.clientX + "px");
-      document.documentElement.style.setProperty('--mouseXPct', e.clientX / window.innerWidth);
-      document.documentElement.style.setProperty('--mouseY', e.clientY + "px");
+    document.addEventListener("mousemove", e => {
+      document.documentElement.style.setProperty("--mouseX", e.clientX + "px");
+      document.documentElement.style.setProperty(
+        "--mouseXPct",
+        e.clientX / window.innerWidth
+      );
+      document.documentElement.style.setProperty("--mouseY", e.clientY + "px");
     });
 
     if (window.DeviceOrientationEvent) {
-      window.addEventListener('deviceorientation', function(e){
-        document.documentElement.style.setProperty('--mouseXPct', e.gamma/1.8+50);
+      window.addEventListener("deviceorientation", function(e) {
+        document.documentElement.style.setProperty(
+          "--mouseXPct",
+          e.gamma / 1.8 + 50
+        );
       });
     }
-
   }
-}
+};
 </script>
 
 
 <style lang="scss">
-
 html {
   background: rgba(89, 104, 107, 1);
 
-&.index body {
+  &.index body {
     font-size: 18px;
     @media (min-width: 500px) {
       font-size: 20px;
     }
     margin: 0;
     padding: 0;
-    --mousetate: calc((var(--mouseXPct) - .5) * 10);
+    --mousetate: calc((var(--mouseXPct) - 0.5) * 10);
     background: linear-gradient(
-      calc( var(--mousetate) * 5deg),
+      calc(var(--mousetate) * 5deg),
       rgba(0, 0, 0, 1) 0%,
       rgb(50, 50, 80) 0%,
       rgba(89, 104, 107, 1) 100%
@@ -158,9 +190,8 @@ html {
 <style lang="scss" scoped>
 /* some variables for y'all */
 
-$font-fam-base: "PT Serif", 'Times New Roman', Times, serif;
+$font-fam-base: "PT Serif", "Times New Roman", Times, serif;
 $font-fam-display: "Open Sans", Helvetica, Arial, sans-serif;
-
 
 /* basic reset and typo */
 ul {
@@ -168,16 +199,18 @@ ul {
   padding-left: 1em;
 }
 
-
-
-h1, h2, h3, h4, h5 {
+h1,
+h2,
+h3,
+h4,
+h5 {
   font-family: $font-fam-display;
 }
 
-p, li {
-  font-family: $font-fam-base; 
+p,
+li {
+  font-family: $font-fam-base;
 }
-
 
 a {
   color: rgba(255, 255, 255, 0.7);
@@ -202,7 +235,6 @@ a {
   }
 }
 
-
 .site-intro {
   top: 50px;
   left: calc(10vw + 5px);
@@ -214,7 +246,6 @@ a {
     max-width: calc(90vw - 36px);
   }
 
-  
   &:before {
     content: "";
     position: absolute;
@@ -223,7 +254,7 @@ a {
     height: 140%;
     left: -10%;
     top: -10%;
-    opacity: .8;
+    opacity: 0.8;
     background: linear-gradient(4deg, black 0%, #202033 80%);
     clip-path: polygon(7% 8%, 100% 0%, 89% 99%, 0% 84%);
     z-index: 0;
@@ -233,8 +264,6 @@ a {
   &.showInfo:before {
     clip-path: polygon(3% 9%, 100% 0%, 82% 100%, 2% 84%);
   }
-
-
 
   > * {
     z-index: 1;
@@ -253,7 +282,8 @@ h1 {
   position: relative;
   z-index: 2;
 
-  .t,.dot {
+  .t,
+  .dot {
     letter-spacing: -1vw;
   }
 }
@@ -273,13 +303,13 @@ h2 {
   --n2: -72px;
   --n3: -108px;
 
-// this works simply nicely.
-  --offset1: calc( var(--n1) * .75 + var(--n1) * var(--scroll));
-  --offset2: calc( var(--n2) * .75 + var(--n2) * var(--scroll));
-  --offset3: calc( var(--n3) * .75 + var(--n3) * var(--scroll));
+  // this works simply nicely.
+  --offset1: calc(var(--n1) * 0.75 + var(--n1) * var(--scroll));
+  --offset2: calc(var(--n2) * 0.75 + var(--n2) * var(--scroll));
+  --offset3: calc(var(--n3) * 0.75 + var(--n3) * var(--scroll));
 
-
-  text-shadow: 0px var(--offset1) 2px, 0px var(--offset2) 4px, 0px var(--offset3) 8px;
+  text-shadow: 0px var(--offset1) 2px, 0px var(--offset2) 4px,
+    0px var(--offset3) 8px;
 }
 
 h3 {
@@ -295,7 +325,7 @@ h3 {
 }
 
 .tagline {
-  font-size: calc(22px + .5vw);
+  font-size: calc(22px + 0.5vw);
   margin: 0;
   color: #fff;
   opacity: 0.5;
@@ -305,7 +335,7 @@ h3 {
     margin-left: 26px;
   }
   span {
-    font-size: calc(16px + .4vw);
+    font-size: calc(16px + 0.4vw);
   }
 }
 
@@ -337,26 +367,25 @@ section {
     height: 140%;
     left: -10%;
     top: -10%;
-    opacity: .8;
+    opacity: 0.8;
     background: linear-gradient(4deg, black 0%, #202033 80%);
     clip-path: polygon(4% 11%, 93% 7%, 90% 87%, 6% 83%);
     z-index: -1;
   }
 }
 
-[class^='item-list'] {
+[class^="item-list"] {
   list-style: none;
   padding: 0;
   opacity: 0.9;
 }
 .item-list-work > li {
   color: rgba(255, 255, 255, 0.5);
-  
+
   margin-bottom: 40px;
 }
 
 .item-list-team {
-  
   left: -40px;
   position: relative;
   padding: 4px 36px;
@@ -398,17 +427,17 @@ button {
   color: #ccc;
   transform: skew(0.02turn, 4deg);
   position: relative;
-  transition: transform .5s;
-  opacity: .75;
+  transition: transform 0.5s;
+  opacity: 0.75;
   cursor: pointer;
 
-  &:before{
-      position:absolute;
-      content:'';
-      top:-10px;
-      right:-10px;
-      left:-10px;
-      bottom:-10px;
+  &:before {
+    position: absolute;
+    content: "";
+    top: -10px;
+    right: -10px;
+    left: -10px;
+    bottom: -10px;
   }
 
   &:focus,
